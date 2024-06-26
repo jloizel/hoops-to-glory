9c845dfc061e59d259aa86e0ef72e530ba2ee916
Jack <jmloizel@gmail.com>
2024-06-26 23:28:04 +0100

    added images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,14 @@ import styles from "./page.module.css"
 
 const Home = () => {
   return (
-    <div className={styles.home}>page</div>
+    <div className={styles.home}>page
+      <div className={styles.imageContainer}>
+        <img src="/logo.png" className={styles.image}></img>
+      </div>
+      <div className={styles.imageContainer}>
+        <img src="/logo2.png" className={styles.image}></img>
+      </div>      
+    </div>
   )
 }
 
