6e37862e1e5be861978829168511d0afffd51657
Jack <jmloizel@gmail.com>
2024-07-01 23:58:48 +0100

    updates

--- components/mechanics/training/training.tsx
+++ components/mechanics/training/training.tsx
@@ -4,11 +4,13 @@ import React, { useState } from 'react';
 import styles from "./page.module.css";
 import { BsConeStriped } from "react-icons/bs";
 import { FaRunning, FaBasketballBall, FaDumbbell } from "react-icons/fa";
+import Animation from './animation';
 
 type TrainingType = 'agility' | 'shooting' | 'fitness';
 
 const Training = () => {
   const [trainingType, setTrainingType] = useState<TrainingType | null>(null);
+  const trainingDuration = 60;
   const [trainingInProgress, setTrainingInProgress] = useState(false);
   const [skills, setSkills] = useState<{ agility: number, shooting: number, fitness: number }>({
     agility: 0,
@@ -28,18 +30,14 @@ const Training = () => {
     }, 60000); // 60 seconds for training
   };
 
-  const totalSkillLevel = skills.agility + skills.shooting + skills.fitness;
+  const averageSkillLevel = (skills.agility + skills.shooting + skills.fitness)/3;
 
-  const getTrainingIcon = () => {
-    if (trainingType === 'agility') {
-      return <FaRunning className={`${styles.progressIcon} ${styles.agilityAnimation}`} />;
-    } else if (trainingType === 'shooting') {
-      return <FaBasketballBall className={`${styles.progressIcon} ${styles.shootingAnimation}`} />;
-    } else if (trainingType === 'fitness') {
-      return <FaDumbbell className={`${styles.progressIcon} ${styles.fitnessAnimation}`} />;
-    }
-    return null;
+  const startTraining = (type: TrainingType) => {
+    setTrainingType(type);
+    setTimeout(() => setTrainingType(null), trainingDuration * 1000);
   };
+
+
 
   return (
     <div className={styles.container}>
@@ -47,48 +45,57 @@ const Training = () => {
         <BsConeStriped className={styles.icon} />
         Training
       </div>
+      <div className={styles.animationContainer}>
+        {trainingType && (
+          <Animation
+            type={trainingType as TrainingType}
+            duration={trainingDuration}
+          />
+        )}
+      </div>
       <div className={styles.content}>
         {/* <span>Select an attribute to train:</span> */}
         <div className={styles.buttonContainer}>
-          <span>Skills & Agility</span>
+          <div className={styles.skill}>
+            <span>Skills & Agility</span>
+            <div>Level: {skills.agility}</div>
+          </div>
           <button
             className={styles.button}
             onClick={() => handleTrainingClick('agility')}
             disabled={trainingInProgress}
           >
-            Train
+            TRAIN
           </button>
         </div>
         <div className={styles.buttonContainer}>
-        <span>Shooting</span>
+          <div className={styles.skill}>
+            <span>Shooting</span>
+            <div>Level: {skills.shooting}</div>
+          </div>
           <button
             className={styles.button}
             onClick={() => handleTrainingClick('shooting')}
             disabled={trainingInProgress}
           >
-            Train
+            TRAIN
           </button>
         </div>
         <div className={styles.buttonContainer}>
-        <span>Fitness & Athleticism</span>
+          <div className={styles.skill}>
+            <span>Fitness & Athleticism</span>
+            <div>Level: {skills.fitness}</div>
+          </div>
           <button
             className={styles.button}
             onClick={() => handleTrainingClick('fitness')}
             disabled={trainingInProgress}
           >
-            Train
+            TRAIN
           </button>
         </div>
-        {trainingInProgress && (
-          <div className={styles.progressContainer}>
-            {getTrainingIcon()}
-          </div>
-        )}
         <div className={styles.skillLevels}>
-          <div>Skills & Agility Level: {skills.agility}</div>
-          <div>Shooting Level: {skills.shooting}</div>
-          <div>Fitness Level: {skills.fitness}</div>
-          <div>Total Skill Level: {totalSkillLevel}</div>
+          <div>Average Skill Level: {averageSkillLevel}</div>
         </div>
       </div>
     </div>
