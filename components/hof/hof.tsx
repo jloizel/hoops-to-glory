5e7d6f4f8bda18f0d7f4492ee23cbf2ea2decce1
Jack <102869405+jloizel@users.noreply.github.com>
2024-08-01 21:05:19 +0100

    adding api call

--- components/hof/hof.tsx
+++ components/hof/hof.tsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./page.module.css"
 import { IoCloseOutline } from "react-icons/io5";
+import { getAllTimes } from '@/app/API';
 
 const HallOfFame = () => {
+  const [loading, setLoading] = useState(false)
+  const [timesFetched, setTimesFetched] = useState(false)
+  const [times, setTimes] = useState("")
+
+  useEffect(() => {
+    const fetchTimes = async () => {
+      if (!timesFetched) {
+      try {
+        setLoading(true)
+        const allTimes = await getAllTimes();
+    
+        // Sort scores by points first, then by time added (more recent first)
+        const sortedTimes = allTimes.sort((a, b) => {
+          if (a.elapsedTime !== b.elapsedTime) {
+            return b.elapsedTime - a.elapsedTime; // Sort by points (higher to lower)
+          } else {
+            // If points are tied, sort by time added (more recent first)
+            return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+          }
+        });
+    
+        setTimes(sortedTimes);
+        setTimesFetched(true);
+      } catch (error) {
+        console.error('Error fetching scores:', error);
+      }
+    }
+    };
+  
+    fetchTimes();
+  }, [times]);
+
   return (
     <div className={styles.container}>
       <span>
--- src/app/API.ts
+++ src/app/API.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosInstance, AxiosResponse } from 'axios';
 
-const BASE_URL = ''; // Update with your backend server URL
+const BASE_URL = 'https://hoopstoglory-server.vercel.app/'; // Update with your backend server URL
 
 // Create an Axios instance with custom configurations
 const api: AxiosInstance = axios.create({
