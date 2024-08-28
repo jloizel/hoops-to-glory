f36face25548a9d44a6ec5fec77c1e23d3b7b72a
Jack <102869405+jloizel@users.noreply.github.com>
2024-08-28 18:57:24 +0100

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           {/* Global site tag (gtag.js) - Google Analytics */}
           <script
             async
-            src="https://www.googletagmanager.com/gtag/js?id=G-3X86Z3FZ9P"
+            src="https://www.googletagmanager.com/gtag/js?id=G-EWN88JJ8QP"
           ></script>
           <script
             dangerouslySetInnerHTML={{
@@ -19,7 +19,7 @@ class MyDocument extends Document {
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-3X86Z3FZ9P');
+                gtag('config', 'G-EWN88JJ8QP');
               `,
             }}
           />
