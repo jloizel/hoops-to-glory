96769c07c5885cdd1d1661ba51d7cbf8e998777c
Jack <jmloizel@gmail.com>
2024-08-30 11:12:08 +0100

    update

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,19 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./page"
 import ReactGA from "react-ga4"
 
-const googleAnalyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID || "";
-ReactGA.initialize("G-DYXXS7VYY4")
+// const googleAnalyticsId = process.env.REACT_APP_GOOGLE_ANALYTICS_ID || "";
+// ReactGA.initialize("G-DYXXS7VYY4")
 
-if (googleAnalyticsId) {
-  // Initialize Google Analytics with your tracking ID
-  ReactGA.initialize(googleAnalyticsId);
+// if (googleAnalyticsId) {
+//   // Initialize Google Analytics with your tracking ID
+//   ReactGA.initialize(googleAnalyticsId);
 
-  // Send a pageview event
-  ReactGA.send({
-    hitType: "pageview",
-    page: window.location.pathname,
-  });
-}
+//   // Send a pageview event
+//   ReactGA.send({
+//     hitType: "pageview",
+//     page: window.location.pathname,
+//   });
+// }
 
 // import TagManager from 'react-gtm-module'
 
