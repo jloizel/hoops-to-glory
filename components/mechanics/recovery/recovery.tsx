b881d1527dac00ead41f950e6b9205c88fb15e35
Jack <jmloizel@gmail.com>
2024-09-23 22:47:13 +0100

    Update recovery.tsx

--- components/mechanics/recovery/recovery.tsx
+++ components/mechanics/recovery/recovery.tsx
@@ -36,7 +36,7 @@ const Recovery: React.FC<RecoveryProps> = ({clickCount, energyLevel, handleClick
   useEffect(() => {
     let interval: NodeJS.Timeout | null = null;
 
-    if (autoClick) {
+    if (autoClick && clickCount > 1) {
       interval = setInterval(() => {
         handleClick();
       }, autoClickInterval);
@@ -45,7 +45,7 @@ const Recovery: React.FC<RecoveryProps> = ({clickCount, energyLevel, handleClick
     return () => {
       if (interval) clearInterval(interval); // Cleanup to prevent multiple intervals
     };
-  }, [autoClickInterval, handleClick]);
+  }, [clickCount]);
   
 
   useEffect(() => {
